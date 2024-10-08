61862b3e9b593dee1a12d5a52287aa04393df4b3
Hammad <hammadazmrauf@gmail.com>
2024-10-08 21:30:05 +0500

    solveCard setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F5F5F5",
+        secondary: "#2A9562",
+      },
+    },
+  },
+  plugins: [],
+};
